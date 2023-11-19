c6dd74c7f2a80855d0bc42f96dd22faf153e7465
itsWindows11 <itswin11@outlook.com>
2023-11-19 18:54:23 +0200

    Updated AGP

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.1.2" apply false
+    id("com.android.application") version "8.1.4" apply false
     id("org.jetbrains.kotlin.android") version "1.9.10" apply false
     id("com.google.protobuf") version "0.9.4" apply false
     kotlin("plugin.serialization") version "1.9.10" apply false
