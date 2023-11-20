629d19707840be86ed5b72c7a70934023139eee3
itsWindows11 <itswin11@outlook.com>
2023-11-20 12:10:10 +0200

    Fixed source generation and exposed the new client through App

--- app/src/main/java/dev/itswin11/greenland/App.kt
+++ app/src/main/java/dev/itswin11/greenland/App.kt
@@ -4,11 +4,18 @@ import android.app.Application
 import android.content.Context
 import androidx.datastore.core.DataStore
 import androidx.datastore.dataStore
-import dev.itswin11.greenland.main.IAtProtoClient
-import dev.itswin11.greenland.main.impl.AtProtoClient
+import dev.itswin11.greenland.api.AtProtoClient
+import dev.itswin11.greenland.api.IAtProtoClient
 import dev.itswin11.greenland.protobuf.AuthInfoContainer
 import dev.itswin11.greenland.serializers.AuthInfoContainerSerializer
 import dev.itswin11.greenland.services.TokenService
+import io.ktor.client.HttpClient
+import io.ktor.client.plugins.HttpRequestRetry
+import io.ktor.client.plugins.HttpTimeout
+import io.ktor.client.plugins.compression.ContentEncoding
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
 
 val Context.authDataStore: DataStore<AuthInfoContainer> by dataStore(
     fileName = "auth.pb",
@@ -18,7 +25,32 @@ val Context.authDataStore: DataStore<AuthInfoContainer> by dataStore(
 class App : Application() {
     companion object {
         lateinit var instance: App
-        val atProtoClient: IAtProtoClient = AtProtoClient("bsky.social")
+
+        val httpClient = createHttpClient()
+        val atProtoClient: IAtProtoClient = AtProtoClient(httpClient)
+
+        private fun createHttpClient(): HttpClient {
+            return HttpClient {
+                install(ContentNegotiation) {
+                    json(Json {
+                        ignoreUnknownKeys = true
+                    })
+                }
+
+                install(HttpRequestRetry) {
+                    retryOnServerErrors(5)
+                }
+
+                install(ContentEncoding) {
+                    gzip()
+                    deflate()
+                }
+
+                install(HttpTimeout) {
+                    requestTimeoutMillis = 20000
+                }
+            }
+        }
     }
 
     init {
