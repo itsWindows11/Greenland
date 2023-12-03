2c717e5d7ac18d6b0a7a1b6aebad3566e4d951bb
itsWindows11 <itswin11@outlook.com>
2023-12-03 16:31:19 +0200

    Updated dependencies and removed temporary workaround

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,7 +51,6 @@ android {
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
-            pickFirsts += "META-INF/versions/9/previous-compilation-data.bin"
         }
     }
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,6 @@ plugins {
     id("com.android.application") version "8.1.4" apply false
     id("org.jetbrains.kotlin.android") version "1.9.10" apply false
     id("com.google.protobuf") version "0.9.4" apply false
-    id("sh.christian.ozone.generator") version "0.0.4" apply false
+    id("sh.christian.ozone.generator") version "0.0.5" apply false
     kotlin("plugin.serialization") version "1.9.10" apply false
 }
